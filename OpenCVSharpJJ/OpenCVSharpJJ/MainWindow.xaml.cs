40ccc14da9665b89d48e5490418246aaaae95009
Jerome Jossent <67312941+jerome-jossent@users.noreply.github.com>
2021-11-14 13:13:47 +0100

    fix projet C# dépendances

--- OpenCVSharpJJ/OpenCVSharpJJ/MainWindow.xaml.cs
+++ OpenCVSharpJJ/OpenCVSharpJJ/MainWindow.xaml.cs
@@ -1,35 +1,4 @@
-﻿using Advise;
-using ImageProcessing;
-using OpenCvSharp;
-using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Text.RegularExpressions;
-using Microsoft.Win32;
-using System.ComponentModel;
-using LiveCharts;
-using LiveCharts.Wpf;
-using LiveCharts.Defaults;
-using OpenCvSharp.Text;
-using System.IO;
-using System.Net;
-using System.IO.Compression;
-using Tesseract;
-
-namespace OpenCVSharpJJ
+﻿namespace OpenCVSharpJJ
 {
     /// <summary>
     /// Logique d'interaction pour MainWindow.xaml
@@ -41,17 +10,6 @@ namespace OpenCVSharpJJ
         {
             InitializeComponent();
 
-        }
-
-        private void btn_global_Click(object sender, RoutedEventArgs e)
-        {
-            Global global = new Global();
-            global.Show();
-            this.Close();
-        }
-
-        private void btn_MesureAmpouleADecanter_Click(object sender, RoutedEventArgs e)
-        {
             MesureAmpouleADecanter mesureAmpouleADecanter = new MesureAmpouleADecanter();
             mesureAmpouleADecanter.Show();
             this.Close();
