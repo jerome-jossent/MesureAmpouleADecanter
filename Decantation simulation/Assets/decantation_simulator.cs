e597de73df372c14e42850f8ad0c02fb4e527e45
jerome jossent <jerome.jossent@veolia.com>
2023-10-09 12:26:20 +0200

    fond noir pour fin | reset & run

--- Decantation simulation/Assets/decantation_simulator.cs
+++ Decantation simulation/Assets/decantation_simulator.cs
@@ -4,36 +4,62 @@ using UnityEngine;
 
 public class decantation_simulator : MonoBehaviour
 {
-    float max, min, value;
-    public float speed_mm_per_sec;
+    public bool run = true;
+    public bool reset = false;
+    float max, min;
+    public float value;
+    float value_prec;
+    public float speed_mm_per_sec = -0.1f;
     public Transform tr;
 
     public float distance_on_screen_cm;
-    public float speed_on_screen_mm_by_sec;
+
+    public float info_speed_on_screen_mm_by_sec;
 
     float t0;
 
     private void Start()
     {
-        tr.localScale = new Vector3(tr.localScale.x, max, tr.localScale.z);
+        min = 0.01f;
+        max = tr.localScale.y;
+        SetToMax();
     }
 
     private void OnEnable()
     {
-        t0=Time.time;
+        t0 = Time.time;
+    }
+
+    void SetToMax()
+    {
+        tr.localScale = new Vector3(tr.localScale.x, max, tr.localScale.z);
     }
 
     void Update()
     {
+        if (reset)
+        {
+            reset = false;
+            SetToMax();
+        }
+
+
+        if (run == false) return;
+
+
         value = tr.localScale.y + speed_mm_per_sec * Time.deltaTime;
+
+        float delta_value = value - value_prec;
+
+        info_speed_on_screen_mm_by_sec = delta_value / Time.deltaTime;
 
         if (value < min)
         {
             value = max;
-            speed_on_screen_mm_by_sec = distance_on_screen_cm * 10 / (Time.time - t0);
-            t0=Time.time;
+            t0 = Time.time;
         }
 
-        tr.localScale = new Vector3 (tr.localScale.x, value, tr.localScale.z);
+        tr.localScale = new Vector3(tr.localScale.x, value, tr.localScale.z);
+        value_prec = value;
     }
 }
